3ccb344be873a9226e004cc5cbc77a57757bd6c8
NicholasKl <19782944@student.curtin.edu.au>
2020-05-05 19:05:44 +0800

    valgrind #1

--- Assignment/lift_sim_A.c
+++ Assignment/lift_sim_A.c
@@ -1,18 +1,62 @@
 #include <stdio.h> 
 #include <stdlib.h> 
-#include <unistd.h>  //Header file for sleep(). man 3 sleep for details. 
+#include <unistd.h>  /*Header file for sleep(). man 3 sleep for details. */
 #include <pthread.h> 
+#include "lift_sim_A.h"
+#include "circularQueue.h"
 
-int main (void)
+CircularQueue* queue;
+
+int main(int argc, char* argv[])
 {
+    /*pthread_t R_id, l1_id, l2_id, l3_id;
+
+    pthread_create(&R_id, NULL, request, (void *)&R_id);
+    pthread_create(&l1_id, NULL, lift, (void *)&l1_id);
+    pthread_create(&l2_id, NULL, lift, (void *)&l2_id);
+    pthread_create(&l3_id, NULL, lift, (void *)&l3_id);
+    pthread_exit(NULL); */
+    entry* ent1;
+    entry* ent2;
+    entry* ent3;
+
+    ent1 = (entry*)malloc(sizeof(entry));
+    ent1->start = 5;
+    ent1->dest = 6;
+
+    ent2 = (entry*)malloc(sizeof(entry));
+    ent2->start = 1;
+    ent2->dest = 3;
+
+    ent3 = (entry*)malloc(sizeof(entry));
+    ent3->start = 9;
+    ent3->dest = 20;
+
+    queue = createCircularQueue(atoi(argv[1]));
+    enqueue(queue, *ent1);
+    enqueue(queue, *ent2);
+    enqueue(queue, *ent3);
+    printQueue(queue);
+    freeQueue(queue);
+
+    free(ent1);
+
     return 0;
 }
 
-void lift(void)
+/* void *request(void* vargp)
 {
-    printf("Hello, this is my thread ID\n");
+    FILE* input = fopen("sim_inpupt", "r");
+    int* id = (int *)vargp;
+    printf("R: Hello, this is my thread ID: %d\n", *id);
+
+    return NULL;
 }
 
-void request(void)
+void *lift(void* vargp)
 {
+    int* id = (int *)vargp;
+    printf("L: Hello, this is my thread ID: %d\n", *id);
+    return NULL;
 }
+*/
---
+++ Assignment/lift_sim_A.h
@@ -0,0 +1,8 @@
+#ifndef LIFTSIMA_H
+#define LIFTSIMA_H
+
+void *myThreadFun(void* vargp);
+void *request(void* vargp);
+void *lift(void* vargp);
+
+#endif
